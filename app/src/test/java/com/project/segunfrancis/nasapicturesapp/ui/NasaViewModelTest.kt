cc1e09fdaaabb0b00bed4956238ca5f5fb75f48b
SegunFrancis <francis4dblues@gmail.com>
2020-11-18 07:58:51 +0100

    Create unit tests
     - Write unit tests for NasaViewModel.kt class

---
+++ app/src/test/java/com/project/segunfrancis/nasapicturesapp/ui/NasaViewModelTest.kt
@@ -0,0 +1,86 @@
+package com.project.segunfrancis.nasapicturesapp.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.project.segunfrancis.data.repository.LocalRepositoryImpl
+import com.project.segunfrancis.domain.repository.LocalRepository
+import com.project.segunfrancis.domain.usecase.GetDataUseCase
+import com.project.segunfrancis.nasapicturesapp.getOrAwaitValue
+import com.project.segunfrancis.nasapicturesapp.mapper.NasaItemMapper
+import com.project.segunfrancis.nasapicturesapp.util.Result
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.testing.*
+import kotlinx.coroutines.CoroutineDispatcher
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.Matchers.not
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.robolectric.annotation.Config
+import java.io.InputStream
+import javax.inject.Inject
+
+/**
+ * Created by SegunFrancis
+ */
+
+@UninstallModules(RepositoryModule::class)
+@HiltAndroidTest
+@Config(application = HiltTestApplication::class)
+@RunWith(AndroidJUnit4::class)
+class NasaViewModelTest {
+
+    @Module
+    @InstallIn(ActivityRetainedComponent::class)
+    class TestRepositoryModule {
+
+        @Provides
+        fun localRepository(localRepositoryImpl: LocalRepositoryImpl): LocalRepository {
+            return mock(LocalRepository::class.java)
+        }
+    }
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @BindValue @JvmField
+    val localRepository: LocalRepository = mock(LocalRepository::class.java)
+
+    @Inject lateinit var getDataUseCase: GetDataUseCase
+    @Inject lateinit var inputStream: InputStream
+    @Inject lateinit var mapper: NasaItemMapper
+    @Inject lateinit var dispatcher: CoroutineDispatcher
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun testGetPictureList() {
+
+        val viewModel = NasaViewModel(getDataUseCase, inputStream, mapper, dispatcher)
+        viewModel.getPictureList()
+        val value = viewModel.pictureList.getOrAwaitValue()
+        assertThat(value, not(nullValue()))
+    }
+
+    @Test
+    fun testGetPictureList_isNotEmpty() {
+
+        val viewModel = NasaViewModel(getDataUseCase, inputStream, mapper, dispatcher)
+        viewModel.getPictureList()
+        val value = viewModel.pictureList.getOrAwaitValue()
+        if (value is Result.Success)
+            assert(value.data.isNotEmpty())
+    }
+}
